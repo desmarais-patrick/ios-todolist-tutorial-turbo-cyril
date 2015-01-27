279e16a28ea8f3b3d37ffef2798aa34e32ef0e52
Patrick Desmarais <patrick.desm@gmail.com>
2015-01-26 19:48:35 -0500

    Reviewed role of main.m; now with ugly comments

--- ToDoList/main.m
+++ ToDoList/main.m
@@ -10,7 +10,18 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
+	// @autoreleasepool handles memory management using ARC (Automatic Reference Counting)
+	// ? If ARC is used, does it detect circular references?
 	@autoreleasepool {
+		// UIApplicationMain starts the app within iOS.
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+
+		// ==> Creates a UIApplication (application object) instance (owns the event loop).
+		//               from UIKit...
+		// HIGH-LEVEL APP BEHAVIORS
+
+		// ==> Creates an AppDelegate (app delagate) (window drawn on)
+		// CUSTOM APP-LEVEL CODE
+		// See AppDelegate.h (interface) and AppDelegate.m (implementation)
 	}
 }
